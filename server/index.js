6c14a4270ae807ff7182fed40e5a8886c9c6f015
wnd-studio <wendellsilvaa012@gmail.com>
2026-08-19 01:23:44 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -38,6 +38,12 @@ if (isProd && !process.env.SESSION_SECRET) {
 }
 
 const app = express();
+
+app.use((req, res, next) => {
+  console.log(`[HTTP] ${req.method} ${req.url}`);
+  next();
+});
+
 app.use(express.json());
 
 // Uma Activity roda dentro de um iframe em <id>.discordsays.com, que por sua
